e327351267c91f8dbe3e19362fac3e99825480e0
Sergio Rivera <sergio_checo1211@hotmail.com>
2018-10-23 13:34:27 -0500

    Se crea interfaz device

---
+++ src/main/java/Tester/Device.java
@@ -0,0 +1,15 @@
+package Tester;
+
+public interface Device 
+{
+	public void nombre();
+	
+	public void turnON();
+	
+	public void play();
+	
+	public void stop();
+	
+	public void turnOFF();
+	
+}
